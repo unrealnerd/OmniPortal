d77da4a41a682f48fefd09664b3b7e162bdb9003
unrealnerd <arjunshetty2020@gmail.com>
2019-06-30 09:59:03 +0530

    * added material library * created a shared module for material * updated home page

--- omni-protal/src/app/app.module.ts
+++ omni-protal/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { AppMaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, BrowserAnimationsModule, AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
